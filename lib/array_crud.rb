2cee533fcd806e5608dd7ee250c5646f69ec9452
Sureyya Celep <sureyyacelep@yahoo.com>
2020-02-02 01:54:49 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -15,11 +15,11 @@ array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
-array.shift  
+array.pop  
 end
 
 def remove_element_from_start_of_array(array)
-array[index_number]  
+array.shift  
 end
 
 def retrieve_element_from_index(array, index_number)
